fd91183f58dd80f1631fb16c03bf62e530e8a3c7
Shirin Heidari <shirin2u@gmail.com>
2022-09-15 18:58:55 +1000

    Add checkStockQuantities to utils.js and use it in fulfillOrders function.

--- src/initiate-order-run.js
+++ src/initiate-order-run.js
@@ -1,3 +1,4 @@
+const data = require("../data.json");
 
 class InitiateOrderRun {
     constructor(orderIds) {
@@ -5,6 +6,21 @@ class InitiateOrderRun {
     }
 
     fulfillOrders() {
+        const { orders, products } = data;
+
+        // Getting the order objects from the data.json file based on the order IDs given as an input
+        const filteredData = orders.filter((data) => this.orderIds.includes(data.orderId));
+
+        // List of unfulfilled orders
+        const unfulfilledOrders = filteredData.filter((order) => {
+            // Checking to see if there is enough in stock to fulfill the order
+            const canBeFulfilled = checkStockQuantities(order, products);
+        
+            return !canBeFulfilled;
+        });
+
+        // Only return the order IDs of the unfulfilled orders
+        return unfulfilledOrders.map((order) => order.orderId);
     }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,10 @@
-function checkStockQuantities() {}
+// This function checks to see if the items are in stock
+exports.checkStockQuantities = (order, products) => {
+    return order.items.every((item) => {
+      const product = products.find((product) => product.productId === item.productId);
+      return product.quantityOnHand >= item.quantity;
+    });
+  };
 
 function updateDate() {}
 
